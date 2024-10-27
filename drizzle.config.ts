96e689f6468e1e5d18a9186235dd0811d9c12706
sukant-pid1 <sukant.thombare@vit.edu.in>
2024-10-27 10:59:53 +0530

    Add files via upload

---
+++ drizzle.config.ts
@@ -0,0 +1,12 @@
+import * as dotenv from "dotenv";
+dotenv.config({ path: ".env" });
+
+import { defineConfig } from "drizzle-kit";
+export default defineConfig({
+  out: "./drizzle",
+  schema: "./src/lib/db/schema.ts",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+  },
+});
